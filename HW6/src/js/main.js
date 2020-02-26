b760551cd6551c286462366c9ff7fe01ca2bdb54
Pavlo <starynskyi@ukr.net>
2020-02-26 11:54:23 +0200

    6 HW

---
+++ HW6/src/js/main.js
@@ -0,0 +1,64 @@
+"use strict";
+
+let beginCalc = document.getElementById("start");
+
+let budget = document.querySelector(".budget-value");
+
+let dayBudget = document.querySelector(".daybudget-value");
+
+let levelVal = document.querySelector(".level-value");
+
+let expVal = document.querySelector(".expenses-value");
+
+let optExpenses = document.querySelector(".optionalexpenses-value");
+
+let incomeValue = document.querySelector(".income-value");
+
+let monthSave = document.querySelector(".monthsavings-value");
+
+let yearSave = document.querySelector(".yearsavings-value");
+
+let expensesItem = document.querySelectorAll(".expenses-item");
+
+let buttonAdoptOne = document.querySelectorAll(".expenses-item-btn");
+
+let buttonCount = document.querySelector(".count-budget-btn");
+
+let optExpensesList = document.querySelectorAll(".optionalexpenses-item");
+
+let incomePotential = document.querySelector(".choose-income");
+
+let savings = document.getElementById("savings");
+
+let sum = document.querySelector(".choose-sum");
+
+let percent = document.querySelector(".choose-percent");
+
+let year = document.querySelector(".year");
+
+let month = document.querySelector(".month");
+
+let day = document.querySelector(".day");
+
+/*console.log(budget);
+console.log(dayBudget);
+console.log(levelVal);
+
+console.log(expVal);
+console.log(optExpenses);
+console.log(incomeValue);
+console.log(monthSave);
+console.log(yearSave);
+console.log(expensesItem);
+console.log(buttonAdoptOne);
+console.log(buttonCount);
+console.log(optExpensesList);
+
+console.log(incomePotential);
+console.log(savings);
+
+console.log(sum);
+console.log(percent);
+console.log(year);
+console.log(month);
+console.log(day);*/
